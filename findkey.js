d4f2689eb91b996a201491d2d1aa7ec0b83e7593
Praneeth <praneethabc123@gmail.com>
2019-10-18 00:07:59 +0000

    created findkey

---
+++ findkey.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+let findKey = function(object,callback){
+  for (let x in object){
+    if(callback(object[x])){
+      return x;
+    }
+    
+  }
+};
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma");
