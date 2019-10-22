8443e9a660ca4962b3e218652766ee6f4775f8c2
Praneeth <praneethabc123@gmail.com>
2019-10-22 00:33:12 +0000

    Adding mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,8 @@
-const assertEqual = require("../assertEqual");
-const tail = require("../tail");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words).length, 3);
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("tail", () => {
+  it(`returns 3 for ["Yo Yo", "Lighthouse","Labs"]`, () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse","Labs"]).length, 3);
+  });
+});
